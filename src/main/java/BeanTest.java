ebfe1a7da0573629e8461b52be9e92942db5398f
Kiley Hector <kiley.hector@gmail.com>
2023-01-03 11:38:28 -0500

    I think i have the base for the java beans done

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,27 @@
+public class BeanTest {
+    private String Album;
+    private String Qoute;
+    private String Author;
+
+
+    public String getAlbum() {
+        return Album;
+    }
+    public  void setAlbum(){
+        this.Album = Album;
+    }
+    public String getQoute(){
+        return Qoute;
+    }
+    public  void setQoute(){
+        this.Qoute = Qoute;
+    }
+
+    public String getAuthor(){
+        return Author;
+    }
+    public void setAuthor(){
+        this.Author = Author;
+    }
+
+}

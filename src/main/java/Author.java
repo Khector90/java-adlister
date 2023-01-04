9beb041e5e88063e88bae761bfc050974f6f1377
Kiley Hector <kiley.hector@gmail.com>
2023-01-04 12:20:30 -0500

    MAde the classes for javabeans

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,14 +1,6 @@
 public class Author {
-    private String Qoute;
+
     private String Author;
-
-    public String getQoute(){
-        return Qoute;
-    }
-    public  void setQoute(){
-        this.Qoute = Qoute;
-    }
-
     public String getAuthor(){
         return Author;
     }
---
+++ src/main/java/Qoutes.java
@@ -0,0 +1,14 @@
+public class Qoutes {
+
+    private String Qoute;
+
+    public String getQoute(){
+        return Qoute;
+    }
+    public  void setQoute(){
+        this.Qoute = Qoute;
+    }
+
+}
+
+

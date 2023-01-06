6e5f5bb81c1599bdcc179cbdb884db5f6259ade9
Kiley Hector <kiley.hector@gmail.com>
2023-01-06 11:16:17 -0500

    I am so so lost.

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,2 +1,21 @@
-public class MySQLAdsDao {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+
+    public MySQLAdsDao(Config config){
+        AdDriver adDriver = new AdDriver();
+        connection = adDriver.getConnection();
+    }
+
+    @Override
+    public List<Ad> all () throws SQLException{
+
+    }
+
 }
